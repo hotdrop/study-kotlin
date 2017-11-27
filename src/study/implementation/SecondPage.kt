4f4714efd16c131c7db58e2acd86209edcbc15a3
kenji <gunsecondstage0106@gmail.com>
2017-11-27 12:39:12 +0900

    Clear all test to drawing book.

--- src/study/implementation/SecondPage.kt
+++ src/study/implementation/SecondPage.kt
@@ -8,11 +8,32 @@ class SecondPage: AbstractStudy(SecondPage::class.java.simpleName) {
     override fun execute() {
         super.execute()
 
-        val targetNo = 1
+        val targetNo = 2
         when(targetNo) {
             1 -> sockMerchant()
+            2 -> drawingBook()
             else -> println("Your set number:'$targetNo' is nothing question.")
         }
+    }
+
+    /**
+     * 両開きの本があって、指定されたページにたどり着くまで
+     * １ページ目から(右へめくると最終ページ目から(左へめくる)めくるページ数が最小の方を求める。
+     * 例えば6ページの本に対し、4ページ目を開け、となったら
+     * 1ページ目から -> 2|3 -> 4|5 で2回
+     * 6ページ目から -> 4|5 で1回
+     * したがって、出力する数値は 1 となる。
+     */
+    private fun drawingBook() {
+        val cin = Scanner(System.`in`)
+        val totalPage = cin.nextInt()
+        val openPage = cin.nextInt()
+
+        val turnPageWithFirst = if(openPage%2 == 0) openPage/2 else (openPage - 1)/2
+        val turnPageWithEnd = (totalPage/2) - turnPageWithFirst
+
+        val minimumTurnPage = if(turnPageWithFirst < turnPageWithEnd) turnPageWithFirst else turnPageWithEnd
+        println(minimumTurnPage)
     }
 
     /**
