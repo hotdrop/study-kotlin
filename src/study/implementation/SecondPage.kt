5a551aae8187df52df697fc44b8966f01d762bb8
kenji <gunsecondstage0106@gmail.com>
2017-12-04 09:50:30 +0900

    Clear all test to cats and mouse.

--- src/study/implementation/SecondPage.kt
+++ src/study/implementation/SecondPage.kt
@@ -8,13 +8,84 @@ class SecondPage: AbstractStudy(SecondPage::class.java.simpleName) {
     override fun execute() {
         super.execute()
 
-        val targetNo = 3
+        val targetNo = 5
         when(targetNo) {
             1 -> sockMerchant()
             2 -> drawingBook()
             3 -> countingValleys()
+            4 -> electronicsShop()
+            5 -> catsAndMouse()
             else -> println("Your set number:'$targetNo' is nothing question.")
         }
+    }
+
+    /**
+     * ２匹の猫が、あるpointX、pointYから同じスピードで移動し、pointZにいるマウスを捕まえる。
+     *   Xにいる猫がマウスを捕まえられる場合「Cat A」
+     *   Yにいる猫がマウスを捕まえられる場合「Cat B」
+     *   XとYが同時にマウスのいるZに行った場合、喧嘩するのでマウスが逃げるため「Mouse C」
+     * と出力する。
+     */
+    private fun catsAndMouse() {
+        val cin = Scanner(System.`in`)
+        val cnt = cin.nextInt()
+
+        (0 until cnt).forEach {
+            judgeWinAnimal(cin.nextInt(), cin.nextInt(), cin.nextInt())
+        }
+    }
+
+    private fun judgeWinAnimal(pointX: Int, pointY: Int, pointZ: Int) {
+        val distanceXtoZ = Math.abs(pointX - pointZ)
+        val distanceYtoZ = Math.abs(pointY - pointZ)
+        when {
+            distanceXtoZ < distanceYtoZ -> println("Cat A")
+            distanceXtoZ > distanceYtoZ -> println("Cat B")
+            else -> println("Mouse C")
+        }
+    }
+
+    /**
+     * あるショップに行ってキーボードとUSBドライブを購入したい。
+     * 入力は5つ「予算」「キーボードの数」「USBドライブの数」「キーボードの価格」「USBドライブの価格」
+     * 予算を最大限使い切る組み合わせの価格を出力する。予算をオーバーする組み合わせしかない場合は −1 を出力する
+     * 例:
+     * 入力
+     *   10 2 3   <- 予算10 キーボード2つ USBドライブ3つ
+     *   3 1      <- キーボードは$3、$1
+     *   5 2 8    <- USBドライブは$5、$2、$8
+     * 出力
+     *   9　　     <- 最大はキーボード$1、USBドライブ$8 で合計$9
+     */
+    private fun electronicsShop() {
+        val cin = Scanner(System.`in`)
+
+        val myMoney = cin.nextInt()
+        val keyboardCnt = cin.nextInt()
+        val usbDriveCnt = cin.nextInt()
+
+        val keyboardCosts = IntArray(keyboardCnt)
+        (0 until keyboardCnt).forEach { keyboardCosts[it] = cin.nextInt() }
+
+        val usbDriveCosts = IntArray(usbDriveCnt)
+        (0 until usbDriveCnt).forEach { usbDriveCosts[it] = cin.nextInt() }
+
+        val maxCost = calcMaxCost(myMoney, keyboardCosts, usbDriveCosts)
+        println(maxCost)
+    }
+
+    private fun calcMaxCost(limitCost: Int, keyboardCosts: IntArray, usbDriveCosts: IntArray): Int {
+        var maxCost = -1
+        // O(n^2)・・・・
+        keyboardCosts.forEach { keyboardCost ->
+            usbDriveCosts.forEach { usbDriveCost ->
+                val pairCost = keyboardCost + usbDriveCost
+                if(pairCost in maxCost..limitCost) {
+                    maxCost = pairCost
+                }
+            }
+        }
+        return maxCost
     }
 
     /**
