b0c260c0ff01d1f28d018dda316c1a96f4f72c16
kenji <gunsecondstage0106@gmail.com>
2018-10-31 22:43:23 +0900

    clear to beautifulDays

--- src/study/implementation/ImplAlgorithms.kt
+++ src/study/implementation/ImplAlgorithms.kt
@@ -7,13 +7,28 @@ class ImplAlgorithms : AbstractStudy(ImplAlgorithms::class.java.simpleName) {
     override fun execute() {
         super.execute()
 
-        val targetNo = 8
+        val targetNo = 1
         when (targetNo) {
-            1 -> bueautifulDaysAtTheMovies()
+            1 -> execBeautifulDaysAtTheMovies()
         }
     }
 
-    private fun bueautifulDaysAtTheMovies() {
+    private fun execBeautifulDaysAtTheMovies() {
+        val res = beautifulDays(20, 23, 6)
+        println(res)
+    }
 
+    private fun beautifulDays(i: Int, j: Int, k: Int): Int {
+
+        fun isBeautiful(day: Int, div: Int): Boolean =
+                day.toString()
+                        .reversed()
+                        .toInt()
+                        .let { ((day - it) % div) == 0 }
+
+        fun countBeautifulDay(startDay: Int, endDay: Int, div: Int) =
+                (startDay..endDay).count { isBeautiful(it, div) }
+
+        return countBeautifulDay(i, j, k)
     }
 }
