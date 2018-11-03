0cd790164051ecdb4ba7f242249cf91c3cd39f15
kenji <gunsecondstage0106@gmail.com>
2018-11-03 10:27:41 +0900

    この問題、nとmのいずれか1の時はsを返せばいいので多少コードを簡略化できた

--- src/study/implementation/ImplAlgorithms.kt
+++ src/study/implementation/ImplAlgorithms.kt
@@ -52,36 +52,32 @@ class ImplAlgorithms : AbstractStudy(ImplAlgorithms::class.java.simpleName) {
     }
 
     fun execSaveThePrisoner() {
-        saveThePrisoner(5, 2, 1).run { if (this != 2) throw IllegalStateException("$this != 2") }
-        saveThePrisoner(7, 19, 2).run { if (this != 6) throw IllegalStateException("$this != 6") }
-        saveThePrisoner(100, 1, 10).run { if (this != 10) throw IllegalStateException("$this != 10") }
-        saveThePrisoner(5, 11, 1).run { if (this != 1) throw IllegalStateException("$this != 1") }
-        saveThePrisoner(1, 5938385, 1).run { if (this != 1) throw IllegalStateException("$this != 1") }
-        saveThePrisoner(784893322, 849791807, 360911386).run { if (this != 425809870) throw IllegalStateException("$this != 425809870") }
-        saveThePrisoner(499999999, 999999997, 2).run { if (this != 499999999) throw IllegalStateException("$this != 499999999") }
-        saveThePrisoner(499999999, 999999998, 2).run { if (this != 1) throw IllegalStateException("$this != 1") }
-        saveThePrisoner(999999999, 999999999 ,1).run { if (this != 999999999) throw IllegalStateException("$this != 999999999") }
-        saveThePrisoner(352926151, 380324688, 94730870).run { if (this != 122129406) throw IllegalStateException("$this != 122129406") }
-        saveThePrisoner(649320641, 742902564, 647542323).run { if (this != 91803604) throw IllegalStateException("$this != 91803604") }
+        saveThePrisoner(5, 2, 1).run { if (this != 2) throw IllegalStateException("case1 $this != 2") }
+        saveThePrisoner(7, 19, 2).run { if (this != 6) throw IllegalStateException("case2 $this != 6") }
+        saveThePrisoner(100, 1, 10).run { if (this != 10) throw IllegalStateException("case3 $this != 10") }
+        saveThePrisoner(5, 11, 1).run { if (this != 1) throw IllegalStateException("case4 $this != 1") }
+        saveThePrisoner(8, 3, 1).run { if (this != 3) throw IllegalStateException("case5 $this != 3") }
+        saveThePrisoner(1, 5938385, 1).run { if (this != 1) throw IllegalStateException("case6 $this != 1") }
+        saveThePrisoner(784893322, 849791807, 360911386).run { if (this != 425809870) throw IllegalStateException("case7 $this != 425809870") }
+        saveThePrisoner(499999999, 999999997, 2).run { if (this != 499999999) throw IllegalStateException("case8 $this != 499999999") }
+        saveThePrisoner(499999999, 999999998, 2).run { if (this != 1) throw IllegalStateException("case9 $this != 1") }
+        saveThePrisoner(999999999, 999999999 ,1).run { if (this != 999999999) throw IllegalStateException("case10 $this != 999999999") }
+        saveThePrisoner(352926151, 380324688, 94730870).run { if (this != 122129406) throw IllegalStateException("case11 $this != 122129406") }
+        saveThePrisoner(649320641, 742902564, 647542323).run { if (this != 91803604) throw IllegalStateException("case12 $this != 91803604") }
     }
 
     private fun saveThePrisoner(n: Int, m: Int, s: Int): Int =
         when {
-            n == 1 -> 1
-            s == 1 -> {
-                when {
-                    n >= m -> m
-                    else -> (m % n).let { if (it == 0) n else it }
-                }
-            }
-            m == 1 -> s
+            m == 1 || n == 1 -> s
+            s == 1 && n >= m -> m
             else -> {
-                (m % n).let {
-                    val num = it + (s - 1)
-                    if (num <= n) {
-                        num
-                    } else {
-                        num - n
+                (m % n).let { rem ->
+                    (rem + (s - 1)).let {
+                        if (it <= n) {
+                            it
+                        } else {
+                            it - n
+                        }
                     }
                 }
             }
